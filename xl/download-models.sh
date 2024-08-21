c19d9fdcff301cf98c94600d057f94d638cf6192
Yevhenii Hrabovskyi <168417527+AIPhilosoph@users.noreply.github.com>
2024-08-21 18:36:22 +0300

    Update download-models.sh

--- xl/download-models.sh
+++ xl/download-models.sh
@@ -81,6 +81,7 @@ EXTENSIONS=(
 CHECKPOINT_MODELS=(
     #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     "https://civitai.com/api/download/models/245598?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+    "https://civitai.com/api/download/models/714092?type=Model&format=SafeTensor&size=full&fp=fp16"
     #"https://huggingface.co/SG161222/RealVisXL_V4.0/resolve/main/RealVisXL_V4.0.safetensors"
     #"https://huggingface.co/RunDiffusion/Juggernaut-X-v10/resolve/main/Juggernaut-X-RunDiffusion-NSFW.safetensors"
     #"https://huggingface.co/RunDiffusion/Juggernaut-XL-v9/resolve/main/Juggernaut-XL_v9_RunDiffusionPhoto_v2.safetensors"
